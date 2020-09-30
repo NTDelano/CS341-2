33805a13048f94f83f28eb2877f154ce9a107f0d
Noah Delano <ntdelano@gmail.com>
2020-09-30 16:00:26 -0700

    Beginning the process of creating software. The goal is to create a program that reads a file, grabs all the values within said file, and display the mean and standard deviation among the list of numbers.

---
+++ RealNumberAnalysis/src/delano/MeanAndDeviation.java
@@ -0,0 +1,48 @@
+package delano;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class MeanAndDeviation {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MeanAndDeviation window = new MeanAndDeviation();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MeanAndDeviation() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 500, 649);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setBounds(10, 10, 466, 25);
+		frame.getContentPane().add(lblNewLabel);
+	}
+}
